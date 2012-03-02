48bbc9da95e7bd63ba3c7581ac485ea0fad73321
Paul O'Connell <paul.t.oconnell@gmail.com>
2012-03-02 14:21:29 +0000

    Add edit url link to the grid.

--- administrator/components/com_bhaa/views/events/tmpl/default_body.php
+++ administrator/components/com_bhaa/views/events/tmpl/default_body.php
@@ -2,16 +2,17 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
 ?>
-<?php foreach($this->items as $i => $item): ?>
+<?php foreach($this->items as $i => $item): 
+	$link = JRoute::_( 'index.php?option=com_bhaa&view=event&layout=edit&id='.$item->id);
+?>
 	<tr class="row<?php echo $i % 2; ?>">
 		<td>
 			<?php echo $item->id; ?>
-		</td>
+		</td>				
 		<td>
-			<?php echo JHtml::_('grid.id', $i, $item->id); ?>
-		</td>
-		<td>
-			<?php echo $item->name; ?> <?php echo $item->alias; ?>
+			<a href="<?php echo $link; ?>" title="<?php echo JText::_('Edit '.$item->alias ); ?>">
+			<?php echo $item->name; ?>
+			</a>
 		</td>
 	</tr>
 <?php endforeach; ?>
